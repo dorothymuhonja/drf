1f41027790c2842b7b2dc2066211955155df6a91
dorothymuhonja <dorothymuhonja7@gmail.com>
2021-04-02 13:07:39 +0300

    all

---
+++ snippets/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer, UserSerializer
+from rest_framework import generics, permissions, renderers, viewsets
+from django.contrib.auth.models import User
+from snippets.permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view, action
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+
+
+
+
+
+class SnippetViewSet(viewsets.ModelViewSet):
+
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly] 
+
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer]) 
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)  
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `retrieve` actions.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer    
+
+
